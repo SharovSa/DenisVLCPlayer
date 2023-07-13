70d30ace9c9c70d0af63bedeedeee1aa3dbf87a7
unknown <vborodulin67@gmail.com>
2023-07-13 12:12:19 +0300

    added songholder metods

--- songholder.py
+++ songholder.py
@@ -1,18 +1,59 @@
 import eyed3
 import os
-
+import song
+import random
 class SongHolder:
+
     def __init__(self):
+        self.songs_array = []
+        self.count = 0
+
+        directory = os.getcwd()
+
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+
+            if filename.endswith(".mp3"):
+
+                file = eyed3.load(filename)
+                title = file.tag.title
+
+                if title is None:
+                    title = filename
+
+                cursong = song.Song(title, file.tag.artist, file.info.time_secs, "-", filename, self.count)
+                self.songs_array.append(cursong)
+                self.count += 1
         pass
 
-        def Checkmemory(self):
-            directory = os.getcwd()
-            for file in os.listdir(directory):
-                filename = os.fsdecode(file)
-                print(filename)
-                if filename.endswith(".mp3"):
-                    self = eyed3.load(filename)
-                    print(self.tag.album)
+    def SongList(self):
+        for i in self.songs_array:
+            print(i.get_name())
+
+
+
+    def Checkmemory(self):
+        directory = os.getcwd()
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".mp3"):
+                self.count += 1
+                file = eyed3.load(filename)
+                title = file.tag.title
+                if title is None:
+                    title = filename
+
+                print(title)
+
+    def GetRandomSong(self):
+        elem = random.randint(0, self.count - 1)
+        return self.songs_array[elem]
+
+    def GetSongById(self, id):
+        if id < 0 or id >= self.count:
+            return self.GetRandomSong()
+        return self.songs_array[id]
 
 pol = SongHolder()
-pol.Checkmemory()
+print(pol.GetRandomSong().get_name())
+print(pol.GetSongById().get_name())
