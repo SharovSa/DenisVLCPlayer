9abcefc9d56276651d178995d2025b2528c98adb
unknown <vborodulin67@gmail.com>
2023-07-13 12:53:26 +0300

    add tests

--- testsongholder.py
+++ testsongholder.py
@@ -1,3 +1,5 @@
 import songholder
-item = songholder.SongHolder
-item.Checkmemory()
+pol = songholder.SongHolder()
+#print(pol.get_random_song().get_name())
+#print(pol.get_song_by_id(2).get_name())
+pol.song_list()
